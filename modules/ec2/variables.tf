de5355e7037487893b659a0793a666aa088d9d84
sanchit1498 <44916333+sanchit1498@users.noreply.github.com>
2024-07-19 22:27:42 -0400

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,14 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.66.1"
-    }
-  }
-  backend "s3" {
-    bucket = "batch3-demo-state-bucket"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "batch3-demo-state-table"
-  }
+variable "ami" {
+  type        = string
+  description = "this will store ami for my instance"
+}
+
+variable "instance_count" {
+  type        = number
+  description = "this is the number of instances you need"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "instance type"
+}
+
+variable "my_env" {
+  type        = string
+  description = "this will store the env for my infra"
 }
