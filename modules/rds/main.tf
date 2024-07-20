12f1cc5d8cb2f6605f2e0c960183f8789d8ba8b9
sanchit1498 <44916333+sanchit1498@users.noreply.github.com>
2024-07-19 22:43:49 -0400

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,14 +1,15 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.66.1"
-    }
-  }
-  backend "s3" {
-    bucket = "batch3-demo-state-bucket"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "batch3-demo-state-table"
+resource "aws_db_instance" "rds_instance" {
+  instance_identifier = var.instance_identifier
+  instance_class      = var.instance_class
+  engine              = var.engine
+  engine_version      = var.engine_version
+  username            = var.username
+  password            = var.password
+  db_name             = var.db_name
+  allocated_storage   = var.allocated_storage
+
+  tags = {
+    Name = "${var.my_env}-rds-instance"
+    env  = var.my_env
   }
 }
