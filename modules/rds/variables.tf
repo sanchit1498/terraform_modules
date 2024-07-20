a111394d5c57b5fe7c72227e7c23023ae0f4a211
sanchit1498 <44916333+sanchit1498@users.noreply.github.com>
2024-07-19 22:44:27 -0400

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,14 +1,44 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.66.1"
-    }
-  }
-  backend "s3" {
-    bucket = "batch3-demo-state-bucket"
-    key = "terraform.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "batch3-demo-state-table"
-  }
+variable "instance_identifier" {
+  type        = string
+  description = "The identifier for the RDS instance"
+}
+
+variable "instance_class" {
+  type        = string
+  description = "The class of the RDS instance"
+}
+
+variable "engine" {
+  type        = string
+  description = "The database engine to use"
+}
+
+variable "engine_version" {
+  type        = string
+  description = "The version of the database engine"
+}
+
+variable "username" {
+  type        = string
+  description = "The username for the RDS instance"
+}
+
+variable "password" {
+  type        = string
+  description = "The password for the RDS instance"
+}
+
+variable "db_name" {
+  type        = string
+  description = "The name of the database"
+}
+
+variable "allocated_storage" {
+  type        = number
+  description = "The allocated storage for the RDS instance in GB"
+}
+
+variable "my_env" {
+  type        = string
+  description = "This will store the environment for the RDS instance"
 }
